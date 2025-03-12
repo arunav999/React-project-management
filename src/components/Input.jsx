830de2e086ceabcc7dcad83bada977a1420304df
Arunav Singh <arunavsingh999@gmail.com>
2025-03-12 10:21:56 +0530

    styling buttons and input with tailwinf css

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
 export default function Input({ label, textarea, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
   return (
     <>
-      <p>
-        <label htmlFor={label}>{label}</label>
+      <p className="flex flex-col gap-1 my-4">
+        <label
+          htmlFor={label}
+          className="text-sm font-bold uppercase  text-stone-500"
+        >
+          {label}
+        </label>
         {!textarea ? (
-          <input id={label} {...props} />
+          <input id={label} {...props} className={classes} />
         ) : (
-          <textarea {...props}></textarea>
+          <textarea {...props} className={classes}></textarea>
         )}
       </p>
     </>
