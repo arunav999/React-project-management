fd07d6274557bca48ecc9baa17fd0753b9c6e697
Arunav Singh <arunavsingh999@gmail.com>
2025-03-12 09:39:52 +0530

    added an input file

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function NewProject() {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <menu>
+        <li>
+          <button>Cancel</button>
+        </li>
+        <li>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>
+        <p>
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" />
+        </p>
+        <p>
+          <label htmlFor="description">Description</label>
+          <textarea name="desc" id="description"></textarea>
+        </p>
+        <p>
+          <label htmlFor="dueDate">Due Date</label>
+          <input type="text" id="dueDate" />
+        </p>
+      </div>
+    </div>
+  );
 }
